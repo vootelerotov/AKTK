910fb687c2575ac8722039cd1079de58cbfbb3e7
Vootele Rotov <vootele.rotov@gmail.com>
2014-05-23 21:05:35 +0300

    Removed Interfaces pakcage

--- src/Interfaces/JavaOutSideBlock.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-/**
- * Created by vootele on 5/22/14.
- */
-public interface JavaOutSideBlock extends Source {
-    String getValue();
-}
--- src/Interfaces/Source.java
+++
@@ -1,9 +0,0 @@
-package Interfaces;
-
-/**
- * Created by vootele on 5/22/14.
- */
-public interface Source {
-
-
-}
